cbe341a39acf1d6df91886e97ac45a353fa74b8f
mikhael <sur.mikhael@gmail.com>
2024-01-15 09:21:25 +0300

    add cat model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveSmallIntegerField()
+    breed = models.CharField(max_length=100)
+    hairiness = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Cat"
+        verbose_name_plural = "Cats"
